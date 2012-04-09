27c3565aae4b2b70189cc0a7b2f174505156dfd9
Ryan McConville <ryan@ryanmcconville.com>
2012-04-09 22:41:14 +0100

    tidy up.. moved functionality in UpdateStoryService class into methods.

--- src/com/ryan/UpdateStoryService.java
+++ src/com/ryan/UpdateStoryService.java
@@ -1,9 +1,5 @@
 package com.ryan;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -20,17 +16,18 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.IBinder;
 import android.text.Html;
 import android.text.Spanned;
 import android.widget.RemoteViews;
 
 public class UpdateStoryService extends Service {
-	private static final String WIKI_FEATURED_ARTICLE_PATH = "https://en.wikipedia.org/w/api.php?action=featuredfeed&feed=featured&feedformat=atom";
-	private String wikiBaseURL = "https://en.wikipedia.org";
 
 	public void onStart(Intent intent, int startID) {
+		final int TITLE_INDEX = 0;
+		final int SUMMARY_INDEX = 1;
+		final String WIKI_FEATURED_ARTICLE_PATH = "https://en.wikipedia.org/w/api.php?action=featuredfeed&feed=featured&feedformat=atom";
+		String wikiBaseURL = "https://en.wikipedia.org";
 
 		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
 				.getApplicationContext());
@@ -46,48 +43,32 @@ public class UpdateStoryService extends Service {
 			RemoteViews views = new RemoteViews(thisWidget.getPackageName(),
 					R.layout.wikiwidgetlayout_background);
 
-			System.out.println("layout is " + views.getLayoutId());
-			URL feedUrl = null;
+			String[] story = parse(WIKI_FEATURED_ARTICLE_PATH);
 
-			try {
-				feedUrl = new URL(WIKI_FEATURED_ARTICLE_PATH);
-			} catch (MalformedURLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+			String storyLink = extractStoryURL(story[SUMMARY_INDEX]);
+
+			String storyURL = null;
+			if (storyLink != null) {
+				storyURL = wikiBaseURL + storyLink;
+			} else {
+				storyURL = wikiBaseURL;
+				System.err
+						.println("Unable to get URL for the complete article");
 			}
-
-			String[] story = parse(feedUrl.toString());
-
-			org.jsoup.nodes.Document doc = Jsoup.parse(story[1]);
-
-			Elements links = doc.select("a[href]");
-			String storyLink = null;
-			for (int z = 0; z < links.size(); z++) {
-				if (links.get(z).hasAttr("href")
-						&& links.get(z).text().equals("more..."))
-					storyLink = links.get(z).attr("href");
-			}
-
-			String storyURL = wikiBaseURL + storyLink;
 
 			Intent browserIntent = new Intent(Intent.ACTION_VIEW,
 					Uri.parse(storyURL));
-			PendingIntent pi = PendingIntent.getActivity(getBaseContext(), 0,
-					browserIntent, 0);
+			PendingIntent openStory = PendingIntent.getActivity(
+					getBaseContext(), 0, browserIntent, 0);
 
-			views.setOnClickPendingIntent(R.id.storySummary, pi);
+			views.setOnClickPendingIntent(R.id.storySummary, openStory);
 
-			Spanned summary = Html.fromHtml(story[1], null, null);
-			String strSummary = summary.toString();
+			Spanned summary = Html.fromHtml(story[SUMMARY_INDEX], null, null);
 
-			strSummary = strSummary.replace('￼', '\0');
-			strSummary = strSummary.replace('\n', '\0');
-			// remove the unicode character that are inserted
-			strSummary = strSummary.replaceAll("[\u0000]", "");
+			String strSummary = cleanupSummary(summary.toString());
 
-			System.out.println("Summary Finished " + strSummary);
 			// update labels
-			views.setTextViewText(R.id.storyHeading, story[0]);
+			views.setTextViewText(R.id.storyHeading, story[TITLE_INDEX]);
 			views.setTextViewText(R.id.storySummary, strSummary);
 
 			// Tell the AppWidgetManager to perform an update on the current app
@@ -100,23 +81,59 @@ public class UpdateStoryService extends Service {
 		System.out.println("Finished service");
 	}
 
+	private String extractStoryURL(String summary) {
+
+		org.jsoup.nodes.Document doc = Jsoup.parse(summary);
+
+		Elements links = doc.select("a[href]");
+		String storyLink = null;
+		for (int z = 0; z < links.size(); z++) {
+			if (links.get(z).hasAttr("href")
+					&& links.get(z).text().equals("more..."))
+				storyLink = links.get(z).attr("href");
+		}
+
+		return storyLink;
+	}
+
+	/**
+	 * Removes unwanted characters from the text
+	 * 
+	 * @param strSummary
+	 *            the Text to perform the replace on
+	 * @return the cleaned String
+	 */
+	private String cleanupSummary(String strSummary) {
+		strSummary = strSummary.replace('￼', '\0');
+		strSummary = strSummary.replace('\n', '\0');
+		// remove the unicode character that are inserted
+		strSummary = strSummary.replaceAll("[\u0000]", "");
+
+		return strSummary;
+
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public String[] parse(String string) {
+	public String[] parse(String urlString) {
+
+		final String HTML_ENTRY_TAG = "entry";
+		final String HTML_TITLE_TAG = "title";
+		final String HTML_SUMMARY_TAG = "summary";
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		String val = null;
 		String summary = null;
 
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document dom = builder.parse(string);
+			Document dom = builder.parse(urlString);
 			Element root = dom.getDocumentElement();
 
-			NodeList items = root.getElementsByTagName("entry");
+			NodeList items = root.getElementsByTagName(HTML_ENTRY_TAG);
 			for (int i = 0; i < items.getLength(); i++) {
 				// stops it breaking < 4.0
 				items.item(i).normalize();
@@ -125,9 +142,9 @@ public class UpdateStoryService extends Service {
 				for (int j = 0; j < properties.getLength(); j++) {
 					Node property = properties.item(j);
 					String name = property.getNodeName();
-					if (name.equalsIgnoreCase("title")) {
+					if (name.equalsIgnoreCase(HTML_TITLE_TAG)) {
 						val = property.getFirstChild().getNodeValue();
-					} else if (name.equalsIgnoreCase("summary")) {
+					} else if (name.equalsIgnoreCase(HTML_SUMMARY_TAG)) {
 						for (int k = 0; k < property.getChildNodes()
 								.getLength(); k++)
 							summary = property.getFirstChild().getNodeValue();
@@ -135,7 +152,7 @@ public class UpdateStoryService extends Service {
 				}
 			}
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			System.err.println("Unable to parse atom feed");
 		}
 		String[] story = { val, summary };
 		return story;
